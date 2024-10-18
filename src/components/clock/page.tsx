b4fb1cef8e2266ee2f4e0cca55e18e4e6b62be5d
Shehroz Hanif <shehrozhanif54@gmail.com>
2024-10-17 17:13:49 -0700

    UPDATE

--- src/components/clock/page.tsx
+++ src/components/clock/page.tsx
@@ -5,7 +5,6 @@
 "use client";
 
 import { useState, useEffect } from 'react';
-import { Button } from "@/components/ui/button";
 import { Switch } from "@/components/ui/switch";
 import { Label } from "@/components/ui/label";
 
